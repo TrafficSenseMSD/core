05bcecbaa279af6853520e08aee91fcdc05fae3e
eilifm <eilif@realtyshares.com>
2017-10-23 01:02:26 -0400

    tutorials continued

---
+++ misc/training_resources/python_training_1/README.md
@@ -0,0 +1,4 @@
+# Python Training 1
+This is a CRASH COURSE.  The intent is to provide a set of tasks during the completion of which 
+you will gain proficiency in Python. 
+
--- misc/training_resources/python_training_1/resources/generate_test_data.py
+++ misc/training_resources/python_training_1/resources/generate_test_data.py
@@ -1,13 +1,29 @@
 import xml.etree.cElementTree as ET
 from xml.dom import minidom
+import random
+import uuid
+from faker import Faker
 
+def make_fake_data():
+    fake = Faker()
 
-data = [
-    {'id' : 1, 'name' : 'jim', 'last_name': 'jones', 'age': 22},
-    {'id' : 2, 'name' : 'jack', 'last_name': 'douglas', 'age': 23},
-    {'id' : 3, 'name' : 'sam', 'last_name': 'jackson', 'age': 24},
-    {'id' : 4, 'name' : 'ralph', 'last_name': 'smith', 'age': 25}
-        ]
+    out = {
+        "id": str(uuid.uuid4()),
+        "name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "age": random.randint(18, 99)
+    }
+    return out
+
+data = [make_fake_data() for i in range(1000)]
+
+# data = [
+#     {'id' : 1, 'name' : 'jim', 'last_name': 'jones', 'age': 22},
+#     {'id' : 2, 'name' : 'jack', 'last_name': 'douglas', 'age': 23},
+#     {'id' : 3, 'name' : 'sam', 'last_name': 'jackson', 'age': 24},
+#     {'id' : 4, 'name' : 'ralph', 'last_name': 'smith', 'age': 25}
+#         ]
+
 
 xml = ET.Element("data")
 
@@ -27,4 +43,5 @@ tree = ET.ElementTree(xml)
 xmlstr = minidom.parseString(ET.tostring(xml)).toprettyxml(indent="   ")
 with open("New_Database.xml", "w") as f:
     f.write(xmlstr)
+
 #tree.write('data.xml')
