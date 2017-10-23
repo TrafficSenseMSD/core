c1c42e7ce31f9217e03f125f4effb572111f5a31
eilifm <eilif@realtyshares.com>
2017-10-23 12:21:26 -0400

    vagrant provisioning

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,4 +2,15 @@
 
 sudo apt-get update
 sudo apt-get -y install git
-sudo apt-get install sumo sumo-tools sumo-doc
+sudo apt-get -y install sumo sumo-tools sumo-doc
+
+# load TrafficSenseMSD core
+mkdir  -p /srv/deploy/current
+(cd /srv/deploy/current && git clone https://github.com/TrafficSenseMSD/core.git)
+(cd /srv/deploy/current && git pull)
+
+# Load eilifm fork of core
+mkdir  -p /srv/dev/eilifm
+(cd /srv/dev/eilifm && git clone https://github.com/eilifm/core.git)
+(cd /srv/dev/eilifm && git pull)
+
