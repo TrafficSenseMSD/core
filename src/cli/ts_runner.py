a09c5ea69299f3cc6535705b854649187e865b07
eilifm <eilif@realtyshares.com>
2017-11-16 12:11:37 -0500

    ts_runner initial work to copy functionality of traci_tls tutorial using the TrafficSense tool chain

--- src/cli/ts_runner.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def main():
-    return
-
-if __name__ == "__main__":
-    main()
-
---
+++ ts_core/cli/ts_runner.py
@@ -0,0 +1,93 @@
+"""
+``ts_runner`` is a WORK IN PROGRESS
+
+"""
+import argparse
+from ts_core.utils.argparse_utils import FullPaths, is_dir, is_file
+
+import sys
+import os
+import random
+import traci, sumolib
+
+
+def run():
+    """execute the TraCI control loop"""
+    step = 0
+    # we start with phase 2 where EW has green
+    traci.trafficlight.setPhase("0", 2)
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        if traci.trafficlight.getPhase("0") == 2:
+            # we are not already switching
+            if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+                # there is a vehicle from the north, switch
+                traci.trafficlight.setPhase("0", 3)
+            else:
+                # otherwise try to keep green for EW
+                traci.trafficlight.setPhase("0", 2)
+        step += 1
+    traci.close()
+    sys.stdout.flush()
+
+
+def generate_routefile():
+    random.seed(42)  # make tests reproducible
+    N = 3600  # number of time steps
+    # demand per second from different directions
+    pWE = 1. / 10
+    pEW = 1. / 11
+    pNS = 1. / 30
+    with open("data/cross.rou.xml", "w") as routes:
+        print("""<routes>
+        <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
+        <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
+
+        <route id="right" edges="51o 1i 2o 52i" />
+        <route id="left" edges="52o 2i 1o 51i" />
+        <route id="down" edges="54o 4i 3o 53i" />""", file=routes)
+        lastVeh = 0
+        vehNr = 0
+        for i in range(N):
+            if random.uniform(0, 1) < pWE:
+                print('    <vehicle id="right_%i" type="typeWE" route="right" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+                lastVeh = i
+            if random.uniform(0, 1) < pEW:
+                print('    <vehicle id="left_%i" type="typeWE" route="left" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+                lastVeh = i
+            if random.uniform(0, 1) < pNS:
+                print('    <vehicle id="down_%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+                lastVeh = i
+        print("</routes>", file=routes)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gui', action='store_false', help='Toggle the use of the SUMO GUI')
+    parser.add_argument('--config_dir', action=FullPaths, type=is_dir, help='Toggle the use of the SUMO GUI')
+    args = parser.parse_args()
+
+    # Checking the binary for
+    if args.gui:
+        sumoBinary = sumolib.checkBinary('sumo')
+    else:
+        sumoBinary = sumolib.checkBinary('sumo-gui')
+
+    generate_routefile()
+
+    traci.start([sumoBinary, "-c", "data/cross.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+
+    run()
+
+    return
+
+if __name__ == "__main__":
+    main()
+
