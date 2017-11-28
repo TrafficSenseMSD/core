fbc3277c3c86b673781a32fc6d68a7e47e15a548
Bryan Healy <bjh2285@rit.edu>
2017-11-28 15:27:55 -0500

    Excel Interface

---
+++ ts_core/utils/ExcelParse/ExcelParse.py
@@ -0,0 +1,40 @@
+import openpyxl as xl
+class ExcelParser():
+	def __init__(self, workbook):
+		self.workbook = xl.load_workbook(filename=workbook)
+		self.worksheet = self.workbook.active
+		self.configuration_index = {
+		'Intersection Shape':'A4',
+		'Route Weights':'A7',
+		'Vehicles':'B2',
+		'Impatience':'C4',
+		'Autonomous Distribution':'C7',
+		'Traffic Demand':'D2',
+		'Accident Response':'E2',
+		'Optimizer':'F2'
+		}
+		
+	def get_worksheet_value(self, title):
+		return self.worksheet[self.configuration_index[title]].value
+	
+	def get_worksheet_values(self):
+		values = {}
+		for config in self.configuration_index.keys():
+			values[config] = self.get_worksheet_value(config)
+		return values
+		
+	def print_worksheet_values(self):
+		values = self.get_worksheet_values()
+		for config in values.keys():
+			print('{}: {}'.format(config,values[config]))
+	
+	def print_sheet_names(self):
+		print(self.workbook.sheetnames)
+		
+		
+		
+		
+		
+if __name__ == "__main__":
+	xlParser = ExcelParser('Configuration.xlsx')
+	xlParser.print_worksheet_values()
