74470071aa42f78215fd82d5e8d806eb881aafd6
eilifm <eilif@realtyshares.com>
2018-04-29 15:10:54 -0400

    tweaking

--- ts_core/cli/ts_config.py
+++ ts_core/cli/ts_config.py
@@ -9,24 +9,23 @@
 The user should understand that, because we have simplified configuration, not all SUMO features are supported 
 out of the box. 
 
+Quickstart
+----------
+Once you've installed.
+
+- Copy ts_core/config/Configuration_template.xlsx to the directory of your choosing.
+- Edit the configuration file to your liking. 
+- From a shell, run ``ts_config build -c <config_path> -p <project_target_dir>``
+
 
 """
 import argparse
 from shutil import copyfile
 from ..excel_parser import parser
 from ..config.config_gen import transform_parsed_excel
-
 from ..utils.argparse_utils import FullPaths, is_dir, is_file
 
 import os
-
-DEBUG = False
-
-SUMO_DIRECTORY = "Fakepath"
-DEFAULT_NODE_FILE = "defaultnodes.xml"
-DEFAULT_EDGE_FILE = "defaultedges.xml"
-NODE_FILE_LOCATION = SUMO_DIRECTORY + "/node.nod.xml"
-EDGE_FILE_LOCATION = SUMO_DIRECTORY + "/edge.edg.xml"
 
 
 def move_file(original_filename, new_filename):
@@ -44,7 +43,19 @@ def move_file(original_filename, new_filename):
     copyfile(original_filename, new_filename)
 
 
-def parse_args():
+def main():
+    """
+    Main function that parses the arguments and
+    will (call functions that )creates all of the required files
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    Nothing
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--project_path", action=FullPaths)
     subparsers = parser.add_subparsers(title="subcommands", help="choose one")
@@ -54,31 +65,10 @@ def parse_args():
     # init.set_defaults(which='init')
 
     build = subparsers.add_parser('build', help="subcommand to build the configuration", description="Run the configuration build.")
-    build.add_argument("-c", "--config", action=FullPaths)#, action=FullPaths)
-
+    build.add_argument("-c", "--config", action=FullPaths, help="Path to Excel config file. Will auto expand relative paths.")#, action=FullPaths)
     build.set_defaults(which='build')
 
-    parser.add_argument("--init", dest="initialize_directory", action='store_true', help="Initializes the directory...")
-    # parser.add_argument("--randomize", dest="randomize", default=False, help="Makes the configs slightly randomized")
-    arguments = parser.parse_args()
-    return arguments
-
-
-
-def main():
-    """
-    Main function that parses the arguments and
-    will (call functions that )creates all of the required files
-
-    Parameters
-    ----------
-    args - ArgParse object with all the requirements
-
-    Returns
-    -------
-    Nothing
-    """
-    args = parse_args()
+    args = parser.parse_args()
 
     if args.which == 'build':
         conf_file_path = args.project_path + "/" + args.config_file
