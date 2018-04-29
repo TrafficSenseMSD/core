76869e088665ebfa70cc8834ff73609b8e84cd79
gns2991 <gns2991@rit.edu>
2018-04-29 03:14:55 -0400

    Updated docstrings - Some file management
    Added the 4 examples shown during ImagineRIT. Foxed Docstrings to use
    numpy format. Removed some unnecessary files introduced in the past.

---
+++ tests/ImagineRIT/rit_static/build.bat
@@ -0,0 +1,2 @@
+#!/bin/bash
+python "/home/gsoponski/sumo-0.32.0/tools/randomTrips.py" -n osm.net.xml --seed 42 --fringe-factor 50 -p 0.722923 -r osm.passenger.rou.xml -o osm.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes 'speedDev="0.1" departLane="best"' --validate
